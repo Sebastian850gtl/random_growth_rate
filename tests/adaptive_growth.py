689eddc8c6803737084405a02c49d567274dd84a
Sebastian <sebastian.baudelet@gmail.com>
2026-02-23 18:40:36 +0100

    Tested sampling OU integral

--- tests/adaptive_growth.py
+++ tests/adaptive_growth.py
@@ -34,11 +34,19 @@ plt.show()
 
 #%% Tests on simulation of \int_0^t C_s ds
 
-def increment_C_intC(Y0, dt, barc, theta, sigma, Nt):
-   
+def increment_C_intC(C0, dt, barc, theta, sigma, Nt):
+    res = np.empty((Nt,2))
+    
     # Definition of relevant constants 
     e = np.exp(- theta* dt)
     
+    # Constant interecept
+    intercept = np.array(( 1 - e, dt  + (e - 1)/theta)) * barc
+    
+    # Linear update
+    lin = np.array( ((e , 0 ), ((1 - e)/theta, 1)) )
+    
+    #Noise, Cholesky matrix
     a = sigma**2 /(2*theta) * (1 -e**2)
     b = sigma**2 /(2 * theta**2) * (1 - e)**2
     c = sigma**2 /theta**2 * (dt -2/theta*(1 -e) + 1/(2*theta)*(1-e**2))
@@ -46,12 +54,71 @@ def increment_C_intC(Y0, dt, barc, theta, sigma, Nt):
     v11 = np.sqrt(a)
     v21 = b/v11
     v22 = np.sqrt(c - v21**2)
+    Cholesky = np.array(( (v11,0),(v21,v22)))
     
     
     # Sampling
-    Z = np.random.radn(Nt,2)
-        
+    Z = np.random.randn(Nt-1,2)
+    dBt = Z.dot(Cholesky.T)
+    
+    #Loop
+    
+    Yk = np.array((C0, 0))
+    res[0,:] = Yk
+    for k in range(Nt-1):
+        Yk = intercept + Yk.dot(lin.T) + dBt[k]
+        res[k+1] = Yk
+    return res
 
+if __name__ == "__main__":
+    Nt = 20
+    dt = 0.05
+    sigma = 1
+    theta = 1
+    barc = 1
+    C0 = 1
+    
+    res = increment_C_intC(C0, dt, barc, theta, sigma, Nt)
+    
+    times = np.linspace(0, (Nt - 1)*dt, Nt)
+    plt.figure(dpi = 200)
+    plt.title("C_t")
+    plt.plot(times,res[:,0])
+    
+    plt.figure(dpi = 200)
+    plt.title("intC_t")
+    plt.plot(times,res[:,1])
+    
+    # Checking mean
+    
+    
+    Nsamples = 10000
+    mean = 0
+    for k in range(Nsamples):
+        res = increment_C_intC(C0, dt, barc, theta, sigma, Nt)
+        mean += res[:,1]
+    mean = mean / Nsamples
+    
+    
+    var = 0
+    for k in range(Nsamples):
+        res = increment_C_intC(C0, dt, barc, theta, sigma, Nt)
+        var += (res[:,1] - mean)**2
+    var = var / (Nsamples - 1)
+    
+    
+    f = lambda t : barc * t
+    plt.figure(dpi = 200)
+    plt.title("Test mean")
+    plt.plot(times,mean)
+    plt.plot(times,f(times))
+    
+    g = lambda t : sigma**2 / theta**2 * ( t - 2/theta * (1 - np.exp(-theta*t)) + 1/(2*theta)*(1 - np.exp(-2*theta * t)))
+    plt.figure(dpi = 200)
+    plt.title("Test variance")
+    plt.plot(times,var)
+    plt.plot(times,g(times))
+        
 
 #%% Reaching desired size
 
