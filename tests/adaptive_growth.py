c5dc7c43ba8fd623dbc2821b6be410a13ae1fcba
Sebastian Baudelet--Aponte Garcia <sbaudele@ptb-11200636.saclay.inria.fr-1>
2026-03-02 14:30:17 +0100

    Adapatative time finding implement start

--- tests/adaptive_growth.py
+++ tests/adaptive_growth.py
@@ -5,28 +5,28 @@ from scipy.stats import norm
 #%% Tests on the approximation for calibrationg delta_t
 
 bar_c = 1
-Cs = 2
+Cs = 1
 theta = 1
 sigma = 1
 
 b = 3
 
 
-Tmax = b / bar_c
+Tmax = b / bar_c /10
 
-Tmin = Tmax / 3
+Tmin = Tmax / 100
 
 t = np.linspace(Tmin,Tmax, 200)
 
 f = lambda t : (b - bar_c * t +  1/theta*(Cs - bar_c)*(1 - np.exp(-theta*t)) ) / ((sigma/theta)*np.sqrt(t - 2/theta *(1 - np.exp(-theta*t)) + 1/(2*theta)*(1 - np.exp(-2*theta*t))))
 
 h = lambda t : (b )/(sigma * t * np.sqrt(t))
-plt.figure(dpi = 200)
-plt.xlabel(r"$\Delta_t$")
-plt.plot(t, f(t))
-plt.plot(t,h(t))
-plt.show()
-
+# print(f(Tmax))
+# plt.figure(dpi = 200)
+# plt.xlabel(r"$\Delta_t$")
+# plt.plot(t, norm.sf(f(t)))
+# plt.plot(t, norm.sf(h(t)))
+# plt.show()
 # epsilon = 0.001
 
 # a = norm.isf(epsilon)
@@ -70,55 +70,113 @@ def increment_C_intC(C0, dt, barc, theta, sigma, Nt):
         res[k+1] = Yk
     return res
 
+def single_increment(Yk, dt, barc, theta, sigma):
+    # Definition of relevant constants 
+    e = np.exp(- theta* dt)
+    
+    # Constant interecept
+    intercept = np.array(( 1 - e, dt  + (e - 1)/theta)) * barc
+    
+    # Linear update
+    lin = np.array( ((e , 0 ), ((1 - e)/theta, 1)) )
+    
+    #Noise, Cholesky matrix
+    a = sigma**2 /(2*theta) * (1 -e**2)
+    b = sigma**2 /(2 * theta**2) * (1 - e)**2
+    c = sigma**2 /theta**2 * (dt -2/theta*(1 -e) + 1/(2*theta)*(1-e**2))
+    
+    v11 = np.sqrt(a)
+    v21 = b/v11
+    v22 = np.sqrt(c - v21**2)
+    Cholesky = np.array(( (v11,0),(v21,v22)))
+    
+    
+    # Sampling
+    Z = np.random.randn(2)
+    dBt = Z.dot(Cholesky.T)
+    
+    #Loop
+    
+    return intercept + Yk.dot(lin.T) + dBt
+
+def find_time_adaptative(b, eps, C0, dt, barc, theta, sigma):
+    tol = norm.isf(eps)
+    print(tol)
+    intC = 0
+    Y = np.array((C0, intC))
+
+    dt = (sigma * tol / b)**(2/3)
+
+    t = 0
+    counter_ite = 0
+    while intC < b:
+        counter_ite += 1
+        dt = ((b - intC )/(sigma* tol))**(2/3)
+        t = t + dt
+
+        Y = single_increment(Y, dt, barc, theta, sigma)
+        intC = Y[1]
+
+
+    return t, intC, counter_ite
+
 if __name__ == "__main__":
     Nt = 20
     dt = 0.05
     sigma = 1
     theta = 1
     barc = 1
-    C0 = 1
+    C0 = -5
     
-    res = increment_C_intC(C0, dt, barc, theta, sigma, Nt)
+    # res = increment_C_intC(C0, dt, barc, theta, sigma, Nt)
     
-    times = np.linspace(0, (Nt - 1)*dt, Nt)
-    plt.figure(dpi = 200)
-    plt.title("C_t")
-    plt.plot(times,res[:,0])
+    # times = np.linspace(0, (Nt - 1)*dt, Nt)
+    # plt.figure(dpi = 200)
+    # plt.title("C_t")
+    # plt.plot(times,res[:,0])
     
-    plt.figure(dpi = 200)
-    plt.title("intC_t")
-    plt.plot(times,res[:,1])
+    # plt.figure(dpi = 200)
+    # plt.title("intC_t")
+    # plt.plot(times,res[:,1])
     
-    # Checking mean
-    
-    
-    Nsamples = 10000
-    mean = 0
-    for k in range(Nsamples):
-        res = increment_C_intC(C0, dt, barc, theta, sigma, Nt)
-        mean += res[:,1]
-    mean = mean / Nsamples
+    # # Checking mean
     
     
-    var = 0
-    for k in range(Nsamples):
-        res = increment_C_intC(C0, dt, barc, theta, sigma, Nt)
-        var += (res[:,1] - mean)**2
-    var = var / (Nsamples - 1)
+    # Nsamples = 10000
+    # mean = 0
+    # for k in range(Nsamples):
+    #     res = increment_C_intC(C0, dt, barc, theta, sigma, Nt)
+    #     mean += res[:,1]
+    # mean = mean / Nsamples
     
     
-    f = lambda t : barc * t
-    plt.figure(dpi = 200)
-    plt.title("Test mean")
-    plt.plot(times,mean)
-    plt.plot(times,f(times))
+    # var = 0
+    # for k in range(Nsamples):
+    #     res = increment_C_intC(C0, dt, barc, theta, sigma, Nt)
+    #     var += (res[:,1] - mean)**2
+    # var = var / (Nsamples - 1)
     
-    g = lambda t : sigma**2 / theta**2 * ( t - 2/theta * (1 - np.exp(-theta*t)) + 1/(2*theta)*(1 - np.exp(-2*theta * t)))
-    plt.figure(dpi = 200)
-    plt.title("Test variance")
-    plt.plot(times,var)
-    plt.plot(times,g(times))
-        
+    
+    # f = lambda t : barc * t
+    # plt.figure(dpi = 200)
+    # plt.title("Test mean")
+    # plt.plot(times,mean)
+    # plt.plot(times,f(times))
+    
+    # g = lambda t : sigma**2 / theta**2 * ( t - 2/theta * (1 - np.exp(-theta*t)) + 1/(2*theta)*(1 - np.exp(-2*theta * t)))
+    # plt.figure(dpi = 200)
+    # plt.title("Test variance")
+    # plt.plot(times,var)
+    # plt.plot(times,g(times))
+    # plt.show()
 
 #%% Reaching desired size
+    sigma = 1
+    theta = 1
+    barc = 1
+    C0 = 8
 
+    b = 3
+    eps = 0.0005
+
+    print(find_time_adaptative(b, eps, C0, dt, barc, theta, sigma))
--- workdir/malthus_constant.py
+++ workdir/malthus_constant.py
@@ -10,39 +10,58 @@ def alive_cells_at_t(t, all_cells, birth_times, division_times):
     idx_birth = bisect.bisect_right(birth_times, t)
 
     # alive if division_time >= t
-    alive = [all_cells[str(i)] for i in range(idx_birth)
+    alive = [all_cells[i] for i in range(idx_birth)
              if division_times[i] >= t]
     return alive
 
 # Case 1 
+np.random.seed(13)
 
 growth_rate = 1
-division_rate_params = offset, alpha = 0, 0
+division_rate_params = alpha, offset = 1, 0
 
 
-Tmax = 1
+Tmax = 11 / growth_rate
+itemax = 1e7
 
-time_points = np.linspace(0, Tmax, 40)
+time_points = np.linspace(Tmax/2, Tmax, 19)
 init = Cell(0,1)
 Model = GF_equal_mitosis(growth_rate = growth_rate, division_rate_params = division_rate_params)
-all_cells, times = Model.run(Tmax, init)
+all_cells, times = Model.run(Tmax, init, itemax = itemax)
 
 birth_times = [c.birth_time for c in all_cells.values()]
 division_times = [c.division_time for c in all_cells.values()]
 
 mean_sizes = []
+number_of_cells = []
 for t in time_points:
     alive_cells = alive_cells_at_t(t, all_cells, birth_times, division_times)
     cell_sizes_at_t = [c.birth_size* np.exp(growth_rate* (t - c.birth_time)) for c in alive_cells]
 
-    print("Number of cells alive at t = %.2f: %4d "%(t, len(alive_cells)))
+    n = len(alive_cells)
+    print("Number of cells alive at t = %.2f: %4d "%(t, n))
     first_moment = np.sum(cell_sizes_at_t)
+    number_of_cells.append(n)
     mean_sizes.append(first_moment)
 
-print(mean_sizes)
-plt.figure(dpi = 200)
-plt.plot(time_points, np.log(np.array(mean_sizes)))
-plt.plot(time_points, growth_rate * time_points)
-plt.show()
+number_of_cells = np.array(number_of_cells)
+
+log_coeff = (np.log(number_of_cells[1:]) - np.log(number_of_cells[:-1]))/(time_points[1:] - time_points[:-1])
+print("Mean coeff is :%.5f"%(np.mean(log_coeff)))
+print(len(log_coeff))
+
+# plt.figure(dpi = 200)
+# plt.plot(time_points, number_of_cells)
+# plt.plot(time_points, np.exp(growth_rate *time_points))
+
+# plt.figure(dpi = 200)
+# plt.plot(time_points, np.log(number_of_cells))
+# plt.plot(time_points, growth_rate * time_points)
+
+# plt.figure(dpi = 200)
+# plt.plot(time_points[1:], log_coeff)
+# plt.plot(time_points, growth_rate * np.ones(len(time_points)))
+# plt.show()
+
 
 
